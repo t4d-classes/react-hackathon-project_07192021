64c8b459a53166dec243bd6dd82fdc7405611918
Herman McElveen <32115641+Herm20@users.noreply.github.com>
2021-07-22 14:33:51 -0400

    Adding Intake page and working features

---
+++ forms-app/src/pages/LeadIntakePage.js
@@ -0,0 +1,13 @@
+import { useSmallBusinessData } from "../hooks/useSmallBusinessData";
+
+export const LeadIntakePage = () => {
+  
+  const { smallBusinessData } = useSmallBusinessData();
+
+    <div>
+      <header>
+        <h1>Lead Intake</h1>
+      </header>
+      <p>Welcome to the Lead Intake page</p>
+    </div>
+};
