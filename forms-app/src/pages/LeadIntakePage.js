bcc054331db709d786a1f02f129dd29ae6457360
Herman McElveen <32115641+Herm20@users.noreply.github.com>
2021-07-22 14:54:17 -0400

    Adding return to intake oage

--- forms-app/src/pages/LeadIntakePage.js
+++ forms-app/src/pages/LeadIntakePage.js
@@ -4,10 +4,12 @@ export const LeadIntakePage = () => {
   
   const { smallBusinessData } = useSmallBusinessData();
 
+  return(
     <div>
       <header>
         <h1>Lead Intake</h1>
       </header>
       <p>Welcome to the Lead Intake page</p>
     </div>
+  )
 };
